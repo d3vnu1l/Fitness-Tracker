8cc322a7e44bbef2ee240d60526b13483bc2ea47
Unknown <radeushane@gmail.com>
2017-04-23 12:15:44 -0700

    Added eeprom long functions
    functions for write read & update 32-bit long in eeprom memory.

---
+++ eepromUtilities.h
@@ -0,0 +1,17 @@
+// eepromUtilities.h
+
+#ifndef _EEPROMUTILITIES_h
+#define _EEPROMUTILITIES_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+void writeLong(int, long);
+void updateLong(int, long);
+long readLong(long);
+
+#endif
+
